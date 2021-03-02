618b7f560dabffcccd573eb224a8681c804abb63
Jacob Lindgren <jlindgre@redhat.com>
2021-03-02 14:40:37 -0600

    move queue name to constant

--- main.go
+++ main.go
@@ -6,12 +6,15 @@ import (
 	"github.com/segmentio/kafka-go"
 )
 
+// SuperKeyRequestQueue - the queue to listen on for superkey requests
+var SuperKeyRequestQueue = "platform.sources.superkey-requests"
+
 func main() {
 	l.InitLogger()
 	l.Log.Info("SuperKey Worker started.")
 
 	// anonymous function, kinda like passing a block in ruby.
-	messaging.ConsumeWithFunction("platform.sources.superkey-requests", func(msg kafka.Message) {
+	messaging.ConsumeWithFunction(SuperKeyRequestQueue, func(msg kafka.Message) {
 		l.Log.Infof("Started processing message %s", string(msg.Value))
 		ProcessSuperkeyRequest(msg)
 		l.Log.Infof("Finished processing message %s", string(msg.Value))
