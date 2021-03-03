164d1430d7d0faad25a4995cce2012eb12081d9e
Jacob Lindgren <jlindgre@redhat.com>
2021-03-03 11:25:37 -0600

    Move worker logic into `main.go` to make it easier to read

--- main.go
+++ main.go
@@ -1,13 +1,25 @@
 package main
 
 import (
+	"encoding/json"
+	"os"
+
 	l "github.com/redhatinsights/sources-superkey-worker/logger"
 	"github.com/redhatinsights/sources-superkey-worker/messaging"
+	"github.com/redhatinsights/sources-superkey-worker/provider"
+	"github.com/redhatinsights/sources-superkey-worker/superkey"
 	"github.com/segmentio/kafka-go"
 )
 
-// SuperKeyRequestQueue - the queue to listen on for superkey requests
-var SuperKeyRequestQueue = "platform.sources.superkey-requests"
+var (
+	// SuperKeyRequestQueue - the queue to listen on for superkey requests
+	SuperKeyRequestQueue = "platform.sources.superkey-requests"
+
+	// DisableCreation disabled processing `create_application` sk requests
+	DisableCreation = os.Getenv("DISABLE_RESOURCE_CREATION")
+	// DisableDeletion disabled processing `destroy_application` sk requests
+	DisableDeletion = os.Getenv("DISABLE_RESOURCE_DELETION")
+)
 
 func main() {
 	l.InitLogger()
@@ -16,7 +28,119 @@ func main() {
 	// anonymous function, kinda like passing a block in ruby.
 	messaging.ConsumeWithFunction(SuperKeyRequestQueue, func(msg kafka.Message) {
 		l.Log.Infof("Started processing message %s", string(msg.Value))
-		ProcessSuperkeyRequest(msg)
+		processSuperkeyRequest(msg)
 		l.Log.Infof("Finished processing message %s", string(msg.Value))
 	})
+}
+
+// processSuperkeyRequest - processes messages.
+func processSuperkeyRequest(msg kafka.Message) {
+	eventType := getEventType(msg.Headers)
+
+	switch eventType {
+	case "create_application":
+		if DisableCreation == "true" {
+			l.Log.Infof("Skipping create_application request: %v", msg.Value)
+			return
+		}
+
+		l.Log.Info("Processing `create_application` request")
+
+		req, err := parseSuperKeyCreateRequest(msg.Value)
+		if err != nil {
+			l.Log.Warnf("Error parsing request: %v", err)
+			return
+		}
+
+		createResources(req)
+		l.Log.Infof("Finished processing `create_application` request for tenant %v type %v", req.TenantID, req.ApplicationType)
+
+	case "destroy_application":
+		if DisableDeletion == "true" {
+			l.Log.Infof("Skipping destroy_application request: %v", msg.Value)
+			return
+		}
+
+		l.Log.Info("Processing `destroy_application` request")
+		req, err := parseSuperKeyDestroyRequest(msg.Value)
+		if err != nil {
+			l.Log.Warnf("Error parsing request: %v", err)
+			return
+		}
+
+		destroyResources(req)
+		l.Log.Infof("Finished processing `destroy_application` request for GUID %v")
+
+	default:
+		l.Log.Warn("Unknown event_type")
+	}
+}
+
+func createResources(req *superkey.CreateRequest) {
+	l.Log.Infof("Forging request: %v", req)
+	newApp, err := provider.Forge(req)
+	if err != nil {
+		l.Log.Errorf("Error forging request %v, error: %v", req, err)
+		l.Log.Errorf("Tearing down superkey request %v", req)
+
+		errors := provider.TearDown(newApp)
+		if len(errors) != 0 {
+			for _, err := range errors {
+				l.Log.Errorf("Error during teardown: %v", err)
+			}
+		}
+
+		return
+	}
+	l.Log.Infof("Finished Forging request: %v", req)
+
+	err = newApp.CreateInSourcesAPI()
+	if err != nil {
+		l.Log.Errorf("Failed to POST req to sources-api: %v, tearing down.", req)
+		provider.TearDown(newApp)
+	}
+}
+
+func destroyResources(req *superkey.DestroyRequest) {
+	l.Log.Infof("Un-Forging request: %v", req)
+	errors := provider.TearDown(superkey.ReconstructForgedApplication(req))
+	if len(errors) != 0 {
+		for _, err := range errors {
+			l.Log.Errorf("Error during teardown: %v", err)
+		}
+	}
+	l.Log.Infof("Finished Un-Forging request: %v", req)
+}
+
+func parseSuperKeyCreateRequest(value []byte) (*superkey.CreateRequest, error) {
+	request := superkey.CreateRequest{}
+	err := json.Unmarshal(value, &request)
+	if err != nil {
+		return nil, err
+	}
+
+	return &request, nil
+}
+
+func parseSuperKeyDestroyRequest(value []byte) (*superkey.DestroyRequest, error) {
+	request := superkey.DestroyRequest{}
+	err := json.Unmarshal(value, &request)
+	if err != nil {
+		return nil, err
+	}
+
+	return &request, nil
+}
+
+// getEventType - iterates through headers to find the `event_type` header
+// from miq-messaging.
+// returns: event_type value
+func getEventType(headers []kafka.Header) string {
+	for _, header := range headers {
+		if header.Key == "event_type" {
+			return string(header.Value)
+		}
+	}
+
+	return ""
 }
